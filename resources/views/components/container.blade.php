923c3d4de67306094cb83b624306acf6db56ceb6
Philip Clifton <me@philipmclifton.com>
2022-03-11 21:55:15 +0000

    WIP

--- resources/views/components/container.blade.php
+++ resources/views/components/container.blade.php
@@ -1,5 +1,5 @@
 <div {{ $attributes->class([
-    'border border-gray-300 shadow-sm bg-white rounded-xl filament-tables-container space-y-12',
+    'border border-gray-300 shadow-sm bg-white rounded-xl filament-tables-container space-y-10',
     'dark:bg-gray-800 dark:border-gray-700' => config('tables.dark_mode'),
 ]) }}>
     {{ $slot }}
--- resources/views/components/filters/trigger.blade.php
+++ resources/views/components/filters/trigger.blade.php
@@ -1,7 +1,1 @@
-<x-default-button class="h-12 animate-bounce" x-on:click="isOpen = ! isOpen">
-    <x-slot name="icon">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-        </svg>
-    </x-slot>
-    Action selected</x-default-button>
+<x-default-button class="h-12" x-on:click="isOpen = ! isOpen">Filter</x-default-button>
